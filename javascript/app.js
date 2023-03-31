c003fc61515f7cd14c6c827e7357e7e4948aa28d
Muhammadshahzaib0 <129533101+Muhammadshahzaib0@users.noreply.github.com>
2023-03-31 09:00:00 -0700

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,75 @@
+alert("Hi there, Sailor!");
+
+console.log("RAMZAN MUBARAK");
+
+
+var title ="NEW LIST";
+console.log( title);
+
+
+var a = 50;
+var b = 80; 
+var c = 20;
+var x =(a + b) - c
+console.log(x);
+
+
+var a = 200;
+var b = 180; 
+var c = 60;
+var x =(a + b) - c
+console.log(x);
+
+
+var x ="MUHAMMMAD" ;
+var y ="SHAHZAIB";
+
+
+var z = x + y;
+console.log(z)
+
+// **************poss incriment*****************
+
+var a =100;
+a++;
+console.log(a)
+
+
+var b =800;
+b++;
+console.log(b)
+
+// **************poss dicriment*****************
+
+var a =100;
+a--;
+console.log(a)
+
+
+var b =800;
+b--;
+console.log(b)
+
+
+var firstname = prompt("Enter your name", "MUHAMMAD");
+var lastname = prompt("Enter your lastname","SHAHZAIB");
+var city = prompt("Enter city", "Karachi");
+console.log(firstname + " " + lastname + " From " + city);
+
+
+var forme = true;
+
+if (forme == true) {
+    console.log("YES");
+} else{
+    console.log("NO");
+}
+
+
+var foryou = true;
+
+if (foryou == true) {
+    console.log("JAWAN PAKISTAN");
+} else{
+    console.log("ADAM JEE");
+}
